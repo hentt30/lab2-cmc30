807e8528de56c3d67ab63c4abc07364c1af18194
Henrique <hentt30@gmail.com>
2022-10-15 14:04:49 -0300

    aaa

--- solve_linear.py
+++ solve_linear.py
@@ -7,40 +7,29 @@ from xml.dom.minidom import parse, Node
 import numpy as np
 
 faces_vector = []
+faces_p1 = np.empty((0,3), int)
+faces_p2 = np.empty((0,3), int)
+faces_p3 = np.empty((0,3), int)
 all_colors = ['r', 'g', 'b']
 
 
 class Face:
-    def __init__(self, p1: tuple, p2: tuple, p3: tuple, c1: tuple, c2: tuple, c3: tuple):
+    
+    def __init__(self, p1: np.array, p2: np.array, p3: np.array, c1: np.array, c2: np.array, c3: np.array):
         self.id = uuid.uuid4()
-        self.x1 = p1[0]
-        self.y1 = p1[1]
-        self.z1 = p1[2]
-        self.x2 = p2[0]
-        self.y2 = p2[1]
-        self.z2 = p2[2]
-        self.x3 = p3[0]
-        self.y3 = p3[1]
-        self.z3 = p3[2]
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
 
-        self.r1 = c1[0]
-        self.g1 = c1[1]
-        self.b1 = c1[2]
-        self.a1 = c1[3]
-        self.r2 = c2[0]
-        self.g2 = c2[1]
-        self.b2 = c2[2]
-        self.a2 = c2[3]
-        self.r3 = c3[0]
-        self.g3 = c3[1]
-        self.b3 = c3[2]
-        self.a3 = c3[3]
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
 
         self.source = 0
 
-        pr = (self.r1 + self.r2 + self.r3) / 3
-        pg = (self.g1 + self.g2 + self.g3) / 3
-        pb = (self.b1 + self.b2 + self.b3) / 3
+        pr = (self.c1[0] + self.c2[0] + self.c3[0]) / 3.0
+        pg = (self.c1[1] + self.c2[1] + self.c3[1]) / 3.0
+        pb = (self.c1[2] + self.c2[2] + self.c3[2]) / 3.0
         self.reflect = {
             'r': pr,
             'g': pg,
@@ -56,39 +45,30 @@ class Face:
         self.centroid = self.get_centroid()
         self.area = self.get_area()
         self.normal = self.get_normal()
+        self.cache = {}
 
     def get_centroid(self) -> tuple:
         """
         Return the centroid of the triangle
         """
-        return ((self.x1 + self.x2 + self.x3) / 3,
-                (self.y1 + self.y2 + self.y3) / 3,
-                (self.z1 + self.z2 + self.z3) / 3)
+        return (self.p1 + self.p2 + self.p3)/3
 
     def get_area(self) -> float:
         """
         Return the area of the triangle => S = |AxB|*(1/2)
         """
-        a = (self.x2 - self.x1, self.y2 - self.y1, self.z2 - self.z1)
-        b = (self.x3 - self.x1, self.y3 - self.y1, self.z3 - self.z1)
-        s = 0.5 * math.sqrt((a[1] * b[2] - a[2] * b[1]) ** 2 +
-                            (a[0] * b[2] - a[2] * b[0]) ** 2 +
-                            (a[0] * b[1] - a[1] * b[0]) ** 2)
-        return s
+        a = self.p2 - self.p1
+        b = self.p3 - self.p1
+        return 0.5 * np.linalg.norm(np.cross(a,b))
+        
 
     def get_normal(self) -> tuple:
         """
         Get the normal of the triangle
         """
-        a = (self.x2 - self.x1, self.y2 - self.y1, self.z2 - self.z1)
-        b = (self.x3 - self.x1, self.y3 - self.y1, self.z3 - self.z1)
-        module = self.get_area() * 2
-        if module == 0.0:
-            print(self.p1(), self.p2(), self.p3())
-        norm = ((a[1] * b[2] - a[2] * b[1]) / module,
-                -(a[0] * b[2] - a[2] * b[0]) / module,
-                (a[0] * b[1] - a[1] * b[0]) / module)
-        return norm
+        a = self.p2 - self.p1
+        b = self.p3 - self.p1
+        return np.cross(a,b)/(np.linalg.norm(np.cross(a,b)))
 
     def unit_vector(self, vector):
         return vector / np.linalg.norm(vector)
@@ -99,34 +79,28 @@ class Face:
         return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
     def determinant_3x3(self, m):
-        return (m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) -
-                m[1][0] * (m[0][1] * m[2][2] - m[0][2] * m[2][1]) +
-                m[2][0] * (m[0][1] * m[1][2] - m[0][2] * m[1][1]))
+        return np.linalg.det(m)
 
     def subtract(self, a, b):
-        return (a[0] - b[0],
-                a[1] - b[1],
-                a[2] - b[2])
+        return a-b
 
     def add(self, a, b):
-        return (a[0] + b[0],
-                a[1] + b[1],
-                a[2] + b[2])
+        return  a + b
 
     def multiply(self, k, b):
-        return (k * b[0], k * b[1], k * b[2])
+        return k*b
 
     def dot_product(self, a, b):
-        return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+        return np.dot(a, b)
 
     def cross_product(self, a, b):
-        return ((a[1]*b[2] - a[2]*b[1]), -(a[0]*b[2]-a[2]*b[0]), (a[0]*b[1] - a[1]*b[0]))
+        return np.cross(a, b)
 
     def tetrahedron_calc_volume(self, a, b, c, d):
-        return (self.determinant_3x3((
-            self.subtract(a, b),
-            self.subtract(b, c),
-            self.subtract(c, d),
+        return (self.determinant_3x3(np.array([
+            a-b,
+            b-c,
+            c-d,]
         )) / 6.0)
 
     def intersect_segment_plane(self, p1, p2, plane_point, plane_normal, epsilon=1e-6):
@@ -139,71 +113,62 @@ class Face:
         vij = self.subtract(p1, p2)
         dot_p_normal_vij = self.dot_product(vij, plane_normal)
 
-        if abs(dot_p_normal_vij) > epsilon:
-            w = self.subtract(p1, plane_point)
-            fac = -self.dot_product(w, plane_normal) / dot_p_normal_vij
-            u = self.multiply(fac, vij)
-            return self.add(p1, u)
+        condition = ~(abs(dot_p_normal_vij) > epsilon)
+        w = self.subtract(p1, plane_point)
+        fac = -self.dot_product(w, plane_normal) / (dot_p_normal_vij + int(condition))
+        u = self.multiply(fac, vij)
+        res = self.add(p1, u)
 
-        return None
+        return np.where(condition,None,res)
 
     def same_side(self, p1, p2, a, b):
         cp1 = self.cross_product(self.subtract(b, a), self.subtract(p1, a))
         cp2 = self.cross_product(self.subtract(b, a), self.subtract(p2, a))
-        if self.dot_product(cp1, cp2) >= 0:
-            return True
-        else:
-            return False
+        return self.dot_product(cp1, cp2) >= 0
 
     def point_in_triangle(self, p, a, b, c) -> bool:
-        if self.same_side(p, a, b, c) and self.same_side(p, b, a, c) and self.same_side(p, c, a, b):
-            return True
-        else:
-            return False
+        return self.same_side(p, a, b, c) and self.same_side(p, b, a, c) and self.same_side(p, c, a, b)
+    
+    
 
-    def p1(self) -> tuple():
-        return (self.x1, self.y1, self.z1)
-
-    def p2(self) -> tuple():
-        return (self.x2, self.y2, self.z2)
-
-    def p3(self) -> tuple():
-        return (self.x3, self.y3, self.z3)
-
-    def get_factor_form(self, face: Face) -> float:
+    def get_view_factor(self, face: Face) -> float:
         """
         return the factor form used to solve the linear system
         """
+        if face.id in self.cache:
+            return self.cache[face.id]
         vij = (self.centroid[0] - face.centroid[0], self.centroid[1] -
                face.centroid[1], self.centroid[2] - face.centroid[2])
         if self.id == face.id or (180.0/math.pi) * self.angle_between(vij, face.normal) < 90:
             return 0
 
-        # check if the triangles aren't blocked
-        for other_face in faces_vector:
-            if other_face.id == face.id or other_face.id == self.id:
-                continue
-            v1 = self.tetrahedron_calc_volume((other_face.x1, other_face.y1, other_face.z1), (
-                other_face.x2, other_face.y2, other_face.z2), (other_face.x3, other_face.y3, other_face.z3), self.centroid)
-            v2 = self.tetrahedron_calc_volume((other_face.x1, other_face.y1, other_face.z1), (
-                other_face.x2, other_face.y2, other_face.z2), (other_face.x3, other_face.y3, other_face.z3), face.centroid)
-            if np.sign(v1) == np.sign(v2):
-                continue
-            intersect = self.intersect_segment_plane(
-                self.centroid, face.centroid, (other_face.x1, other_face.y1, other_face.z1), other_face.normal)
-            if intersect is not None:
-                result = self.point_in_triangle(
-                    intersect, other_face.p1(), other_face.p2(), other_face.p3())
-                if result:
-                    return 0
+        def intersect_line_triangle(q1,q2,p1,p2,p3):
+            def signed_tetra_volume(a,b,c,d):
+                return np.sign(np.sum(np.cross(b-a,c-a)*(d-a),axis=1)/6.0)
 
+            s1 = signed_tetra_volume(q1,p1,p2,p3)
+            s2 = signed_tetra_volume(q2,p1,p2,p3)
+            s3 = signed_tetra_volume(q1,q2,p1,p2)
+            s4 = signed_tetra_volume(q1,q2,p2,p3)
+            s5 = signed_tetra_volume(q1,q2,p3,p1)
+            x = (s1!=s2)
+            y = (s3==s4)
+            z = (s4==s5)
+            return x&y&z
+
+        self_centroid = np.array([self.centroid]*len(faces_p1))
+        face_centroid = np.array([face.centroid]*len(faces_p1))
+        res = intersect_line_triangle(self_centroid,face_centroid,faces_p1,faces_p2,faces_p3)
+        
+        if(np.sum(res) > 2):
+            return 0
         # Return the view factor
         Aj = face.area
         cos_theta_i = np.cos(self.angle_between(vij, self.centroid))
         cos_theta_j = np.cos(self.angle_between(vij, face.centroid))
         r = np.linalg.norm(vij)
-
-        return (Aj * cos_theta_i * cos_theta_j) / (math.pi * (r ** 2) + Aj)
+        self.cache[face.id] = (Aj * cos_theta_i * cos_theta_j) / (math.pi * (r ** 2) + Aj)
+        return self.cache[face.id]
 
 
 def set_id_attribute(parent, attribute_name="id"):
@@ -224,7 +189,7 @@ def set_final_iluminations(faces: list[Face], color: str) -> np.ndarray:
     for i in range(size):
         b[i] = faces[i].source
         for j in range(size):
-            F = faces[i].get_factor_form(faces[j])
+            F = faces[i].get_view_factor(faces[j])
             a[i][j] -= faces[i].reflect[color] * F
     x = np.linalg.solve(a, b)
     for i in range(len(x)):
@@ -236,22 +201,6 @@ if __name__ == "__main__":
     document = parse('exemplocena.dae')
     set_id_attribute(document)
     root = document.documentElement
-    # tree = ET.parse('exemplocena.dae')
-    # root = tree.getroot()
-    # elm = root.find('library_geometries')
-    # objects = {}
-    # for geometry in elm:
-    #     print(geometry.attrib)
-    #     objects[geometry.attrib['id']] = {}
-    #     elements = geometry[0].findall('source')
-    #     pos = list(filter(
-    #         lambda x: x.attrib['id'] == geometry.attrib['id'] + '-positions',
-    #         elements))[0]
-    #     print(pos.find('float_array').text)
-
-    #     triangles = geometry[0].find('triangles')
-    #     p = triangles.find('p')
-    #     indexes = list(map(int, p.text.split()))
 
     objects = {}
     reverse = {}
@@ -286,37 +235,40 @@ if __name__ == "__main__":
         i = 0
         while i < len(indexes):
             j = 3 * indexes[i]
-            p1 = vertices[j], vertices[j + 1], vertices[j + 2]
+            p1 = np.array([vertices[j], vertices[j + 1], vertices[j + 2]])
             k = 3 * indexes[i + 4]
-            p2 = vertices[k], vertices[k + 1], vertices[k + 2]
+            p2 = np.array([vertices[k], vertices[k + 1], vertices[k + 2]])
             m = 3 * indexes[i + 8]
-            p3 = vertices[m], vertices[m + 1], vertices[m + 2]
+            p3 = np.array([vertices[m], vertices[m + 1], vertices[m + 2]])
 
             j = 4 * indexes[i + 3]
-            c1 = colors[j], colors[j + 1], colors[j + 2], colors[j + 3]
+            c1 = np.array([colors[j], colors[j + 1], colors[j + 2], colors[j + 3]])
             k = 4 * indexes[i + 7]
-            c2 = colors[k], colors[k + 1], colors[k + 2], colors[k + 3]
+            c2 = np.array([colors[k], colors[k + 1], colors[k + 2], colors[k + 3]])
             m = 4 * indexes[i + 11]
-            c3 = colors[m], colors[m + 1], colors[m + 2], colors[m + 3]
-
+            c3 = np.array([colors[m], colors[m + 1], colors[m + 2], colors[m + 3]])
+            source = 1 if id == 'Cube' else 0
             face = Face(p1, p2, p3, c1, c2, c3)
-            reverse[id][p1.__str__()] = (face, 1, i)
-            reverse[id][p2.__str__()] = (face, 2, i)
-            reverse[id][p3.__str__()] = (face, 3, i)
+            face.source = source
+            if p1.__str__() not in reverse[id]:
+                reverse[id][p1.__str__()] = []
+            if p2.__str__() not in reverse[id]:
+                reverse[id][p2.__str__()] = []
+            if p3.__str__() not in reverse[id]:
+                reverse[id][p3.__str__()] = []
+            reverse[id][p1.__str__()].append((face, 1, i))
+            reverse[id][p2.__str__()].append((face, 2, i))
+            reverse[id][p3.__str__()].append((face, 3, i))
             object['faces'].append(face)
             faces_vector.append(face)
+            faces_p1=np.append(faces_p1,np.array([p1]),axis=0)
+            faces_p2=np.append(faces_p2,np.array([p2]),axis=0)
+            faces_p3=np.append(faces_p3,np.array([p3]),axis=0)
 
             i += 12
 
-    # Solve the linear system (ax = b) for each color
-    # threads = []
     for c in all_colors:
         set_final_iluminations(faces_vector, c)
-        # t = Thread(target=set_final_iluminations, args=(faces_vector, c))
-        # t.start()
-        # threads.append(t)
-    # for t in threads:
-        # t.join()
 
     # Updates the vertices colors of the faces
     for obj in objects:
@@ -324,11 +276,11 @@ if __name__ == "__main__":
             n = len(faces)
             rm = gm = bm = 0
             for face in faces:
-                rm += face[0]['r' + str(face[1])] / n
-                gm += face[0]['g' + str(face[1])] / n
-                bm += face[0]['b' + str(face[1])] / n
+                rm += getattr(face[0],'r' + str(face[1])) / n
+                gm += getattr(face[0],'g' + str(face[1])) / n
+                bm += getattr(face[0],'b' + str(face[1])) / n
             for face in faces:
-                j = indexes[face[2] + 3 + 4 * (face[1] - 1)]
+                j = 4*indexes[face[2] + 3 + 4 * (face[1] - 1)]
                 objects[obj]['colors'][j] = rm
                 objects[obj]['colors'][j + 1] = gm
                 objects[obj]['colors'][j + 2] = bm
@@ -341,10 +293,9 @@ if __name__ == "__main__":
         for arr in geometry.getElementsByTagName("float_array"):
             id = arr.getAttribute("id")
             splitted_id = id.split('-')
+            if splitted_id[2] != 'colors':
+                continue
             arr.firstChild.data = ' '.join([str(x) for x in objects[splitted_id[0]][splitted_id[2]]])
-        for arr in geometry.getElementsByTagName("p"):
-            splitted_id = id.split('-')
-            arr.firstChild.data = ' '.join([str(x) for x in objects[splitted_id[0]]['mapping']])
 
     with open('solver.dae','w') as file:
         file.write(root.toxml())
